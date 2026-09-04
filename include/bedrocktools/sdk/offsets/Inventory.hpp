95cde768c3a303588146385528f62a061c5d83a5
VENOM P2 GM <abdullahshoieb90@gmail.com>
2026-09-04 12:23:30 +0300

    Create Inventory.hpp

---
+++ include/bedrocktools/sdk/offsets/Inventory.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstddef>
+
+namespace bedrocktools::sdk::offsets::Inventory {
+
+inline constexpr std::size_t FillingContainerItems = 0x140;
+inline constexpr std::size_t ItemStackSize = 0x98;
+
+}
