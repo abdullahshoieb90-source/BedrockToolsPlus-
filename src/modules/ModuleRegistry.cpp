3fccc02382c7af0cf4d8d409f5f4cbf615489121
VENOM P2 GM <abdullahshoieb90@gmail.com>
2026-09-19 16:16:41 +0300

    Update ModuleRegistry.cpp

--- src/modules/ModuleRegistry.cpp
+++ src/modules/ModuleRegistry.cpp
@@ -11,6 +11,7 @@
 #include "hud/hotbarslots.hpp"
 #include "hud/inventoryhud.hpp"
 #include "hud/armorhud.hpp"
+#include "player/ChestStealer.hpp"
 
 
 ModuleRegistry& ModuleRegistry::get() {
@@ -78,4 +79,5 @@ void registerAllModules() {
     registry.emplace<HotbarSlotsModule>();
     registry.emplace<InventoryHudModule>();
     registry.emplace<ArmorModule>();
+    registry.emplace<ChestStealermodule>();
 }
